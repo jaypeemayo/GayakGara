6742c3b770bafa911c7a1e63936134c72d22d5ae
jaype <jaypeemayo@gmail.com>
2016-12-18 18:18:47 +0800

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls:['app.component.css']
 })
 export class AppComponent  { name = 'Angular'; }
